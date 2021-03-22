69aae914a85db84b45d07467203c383e16f50626
Adversariusss <adversariussv@mail.ru>
2021-03-22 10:41:07 +0300

    _

--- bot_v2.py
+++ bot_v2.py
@@ -1,12 +1,9 @@
 import logging
 import random
-import requests
 from aiogram import Bot, Dispatcher, executor, types
 from pars_film import film_rait, film_name
 from all_film import all_film
 from aiogram.dispatcher.filters import Text
-
-# from aiogram.filters import Text
 
 # Создание бота и вызов
 bot = Bot(token="1626448931:AAE5qbxP9aJ3w6SXfINj2Rn8NC1AaVYSaVM")
@@ -34,19 +31,24 @@ async def cmd_start(message):
     await message.answer(f"Привет {message.chat.first_name}, а ты раскабанел")
     await cmd_help(message)
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    buttons = ["Фильм из топ 250 рейтинга IMDb", "Фильм из личного топа владельца", "Пока не нужно советовать фйильм"]
+    # buttons = ["Фильм из топ 250 рейтинга IMDb", "Фильм из личного топа владельца", "Пока не нужно советовать фйильм"]
+    buttons = ["Фильм из личного топа владельца"]
     keyboard.add(*buttons)
     await message.answer("Я могу посоветовать тебе фильм.", reply_markup=keyboard)
 
-"""
-@dp.message_handler(lambda message: message.text == "Фильм топ 250 рейтинга IMDb")
-async def with_film(message: types.Message):
-    await message.answer(film_imdb()) #film_imdb(message)
 
 """
+@dp.message_handler(Text(equals="Фильм топ 250 рейтинга IMDb"))
+async def with_film(message: types.Message):
+    z = str(film_imdb('/filmdb'))
+    await message.answer(text=z) #film_imdb(message)
+"""
+
+
 @dp.message_handler(Text(equals="Фильм из личного топа владельца"))
 async def with_admin(message: types.Message):
     await message.answer(random.choice(all_film))
+
 
 """
 @dp.message_handler(Text(equals="Пока не нужно советовать фильм"))
@@ -54,6 +56,8 @@ async def not_film(message: types.Message):
     if message.text == 'Пока не нужно советовать фильм':
         await cmd_help(message)
 """
+
+
 @dp.message_handler(commands=['filmdb'])
 async def film_imdb(message):
     x = random.randint(0, 249)
@@ -65,6 +69,7 @@ async def film_imdb(message):
         await message.answer(text=f"{film_name[x][4:]} {film_rait[x]} IMDb")
 
 
+"""
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def send_text(message):
     text = message.text
@@ -74,8 +79,7 @@ async def send_text(message):
         pass
         # await cmd_help(message)
         # await message.reply(text=text)
-
-
+"""
 
 
 @dp.message_handler(commands=['filmadmin'])
